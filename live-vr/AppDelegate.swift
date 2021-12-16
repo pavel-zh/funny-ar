bb590938b99c8233b038a5888e8f4e503a4228b7
Bitrise CI <7429748+pavel-zh@users.noreply.github.com>
2021-12-16 17:23:43 -0500

    minorish cleanup

--- live-vr/AppDelegate.swift
+++ live-vr/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,6 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
